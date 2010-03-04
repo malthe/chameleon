80a685ab9976f031eb63e516a9881cb1314c9a9a
Malthe Borch mborch@gmail.com <Malthe Borch mborch@gmail.com@8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9>
2010-03-04 11:21:32 +0000

    Failing test (contributed by Daniel Kraft).
    git-svn-id: svn+ssh://svn.repoze.org/svn/chameleon/trunk@8327 8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9

--- src/chameleon/tests/test_unicode.py
+++ src/chameleon/tests/test_unicode.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 import unittest
 
 class TestUnicodeMisc(unittest.TestCase):
@@ -9,4 +10,23 @@ class TestUnicodeMisc(unittest.TestCase):
         result = t().decode('utf-8')
         self.assertEqual(result,
                          u'Hello World!\nTesting utf-8: \xe4\xf6\xfc\xdf\n')
-        
+
+    def test_utf8_values_in_page_text_template_file(self):
+        import os
+        path = os.path.join(os.path.dirname(__file__), "templates")
+        from chameleon.zpt.template import PageTextTemplateFile
+        t = PageTextTemplateFile(os.path.join(path, 'uglyworld.txt'))
+        value = u'ä'.encode("utf-8")
+        result = t(value=value).decode('utf-8')
+        self.assertEqual(result,
+                u'Inserting a value: ä\n')
+
+    def test_unicode_values_in_page_text_template_file(self):
+        import os
+        path = os.path.join(os.path.dirname(__file__), "templates")
+        from chameleon.zpt.template import PageTextTemplateFile
+        t = PageTextTemplateFile(os.path.join(path, 'uglyworld.txt'))
+        value = u'ä'
+        result = t(value=value).decode('utf-8')
+        self.assertEqual(result,
+                u'Inserting a value: ä\n')
