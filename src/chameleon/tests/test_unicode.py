96ad9695932530d86cd3dafafe406b90a3f9e4b8
Malthe Borch mborch@gmail.com <Malthe Borch mborch@gmail.com@8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9>
2010-03-08 08:20:50 +0000

    Make sure these tests fail, not err.
    git-svn-id: svn+ssh://svn.repoze.org/svn/chameleon/trunk@8356 8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9

--- src/chameleon/tests/test_unicode.py
+++ src/chameleon/tests/test_unicode.py
@@ -7,9 +7,14 @@ class TestUnicodeMisc(unittest.TestCase):
         path = os.path.join(os.path.dirname(__file__), "templates")
         from chameleon.zpt.template import PageTextTemplateFile
         t = PageTextTemplateFile(os.path.join(path, 'helloworld.txt'))
-        result = t().decode('utf-8')
-        self.assertEqual(result,
-                         u'Hello World!\nTesting utf-8: \xe4\xf6\xfc\xdf\n')
+        result = t()
+        try:
+            decoded = result.decode('utf-8')
+        except UnicodeEncodeError, e:
+            self.fail(str(e))
+
+        self.assertEqual(
+            decoded, u'Hello World!\nTesting utf-8: \xe4\xf6\xfc\xdf\n')
 
     def test_utf8_values_in_page_text_template_file(self):
         import os
@@ -17,9 +22,14 @@ class TestUnicodeMisc(unittest.TestCase):
         from chameleon.zpt.template import PageTextTemplateFile
         t = PageTextTemplateFile(os.path.join(path, 'uglyworld.txt'))
         value = u'ä'.encode("utf-8")
-        result = t(value=value).decode('utf-8')
-        self.assertEqual(result,
-                u'Inserting a value: ä\n')
+        result = t(value=value)
+        try:
+            decoded = result.decode('utf-8')
+        except UnicodeEncodeError, e:
+            self.fail(str(e))
+
+        self.assertEqual(
+             decoded, u'Inserting a value: ä\n')
 
     def test_unicode_values_in_page_text_template_file(self):
         import os
@@ -27,6 +37,11 @@ class TestUnicodeMisc(unittest.TestCase):
         from chameleon.zpt.template import PageTextTemplateFile
         t = PageTextTemplateFile(os.path.join(path, 'uglyworld.txt'))
         value = u'ä'
-        result = t(value=value).decode('utf-8')
-        self.assertEqual(result,
-                u'Inserting a value: ä\n')
+        result = t(value=value)
+        try:
+            decoded = result.decode('utf-8')
+        except UnicodeEncodeError, e:
+            self.fail(str(e))
+
+        self.assertEqual(
+            decoded, u'Inserting a value: ä\n')
