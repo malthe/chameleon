c2983960cf5d1e1952ed38bb14a01229b6fe90ff
Malthe Borch <mborch@gmail.com>
2011-02-28 18:51:25 +0100

    Fixed classifier.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
        "Development Status :: 4 - Beta",
        "Intended Audience :: Developers",
        "Programming Language :: Python",
-       "Programming Language :: Python :: 2"
+       "Programming Language :: Python :: 2",
        "Programming Language :: Python :: 3",
        "Programming Language :: Python :: 2.5",
        "Programming Language :: Python :: 2.6",
