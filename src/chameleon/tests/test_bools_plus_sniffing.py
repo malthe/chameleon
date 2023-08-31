6cfdb6c50c66d0fc691978451662b96b2795872e
Chris McDonough <chrism@plope.com>
2023-08-31 17:54:36 -0400

    add test file to exercise boolean treatment in conjunction with type sniffing

---
+++ src/chameleon/tests/test_bools_plus_sniffing.py
@@ -0,0 +1,392 @@
+import difflib
+import unittest
+
+from chameleon import PageTemplate
+
+boolean_html_attributes= [
+        # From http://www.w3.org/TR/xhtml1/#guidelines (C.10)
+        "compact",
+        "nowrap",
+        "ismap",
+        "declare",
+        "noshade",
+        "checked",
+        "disabled",
+        "readonly",
+        "multiple",
+        "selected",
+        "noresize",
+        "defer",
+    ]
+
+xml_bytes = b"""\
+<?xml version="1.0" ?>
+<input type="checkbox" checked="nope" tal:attributes="checked checked" />
+<input type="checkbox" checked="${checked}" />
+"""
+
+xml_w_enc_bytes = b"""\
+<?xml version="1.0" encoding="ascii" ?>
+<input type="checkbox" checked="nope" tal:attributes="checked checked" />
+<input type="checkbox" checked="${checked}" />
+"""
+
+html5_bytes = b"""\
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Title of document</title>
+</head>
+<body>
+  <form>
+    <input type="checkbox" checked="nope"
+           tal:attributes="checked checked" />
+    <input type="checkbox" checked="${checked}" />
+  </form>
+</body>
+</html>
+"""
+
+html5_w_ct_n_enc_bytes = b"""\
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="foo/bar; charset=utf-8" />
+  <title>Title of document</title>
+</head>
+<body>
+  <form>
+    <input type="checkbox" checked="nope"
+           tal:attributes="checked checked" />
+    <input type="checkbox" checked="${checked}" />
+  </form>
+</body>
+</html>
+"""
+
+class BaseTestCase(unittest.TestCase):
+    def get_template(self, text):
+        template = PageTemplate(
+            text,
+            boolean_attributes=boolean_html_attributes
+        )
+        return template
+
+    def get_template_bytes(self):
+        return self.get_template(self.input_bytes)
+
+    def get_template_str(self):
+        return self.get_template(self.input_bytes.decode('utf-8'))
+
+    def assert_same(self, s1, s2):
+        L1 = s1.splitlines()
+        L1 = list(filter(None, [ ' '.join(x.split()).strip() for x in L1 ]))
+        L2 = s2.splitlines()
+        L2 = list(filter(None, [ ' '.join(x.split()).strip() for x in L2 ]))
+        diff = '\n'.join(list(difflib.unified_diff(L1, L2)))
+        assert diff == '', diff
+
+class XMLTestCase(BaseTestCase):
+
+    input_bytes = xml_bytes
+
+    def test_bytes_content_type(self):
+        template = self.get_template_bytes()
+        self.assertEqual(template.content_type, 'text/xml')
+
+    def test_bytes_encoding(self):
+        template = self.get_template_bytes()
+        self.assertEqual(template.encoding, None)
+
+    def test_str_content_type(self):
+        template = self.get_template_str()
+        self.assertEqual(template.content_type, 'text/xml')
+
+    def test_str_encoding(self):
+        template = self.get_template_str()
+        self.assertEqual(template.encoding, None)
+
+    def test_bytes_checked_true(self):
+        template = self.get_template_bytes()
+        expected = """
+        <?xml version="1.0" ?>
+        <input type="checkbox" checked="True" />
+        <input type="checkbox" checked="True" />
+        """
+        result = template(checked=True)
+        self.assert_same(expected, result)
+        
+    def test_bytes_checked_false(self):
+        template = self.get_template_bytes()
+        expected = """
+        <?xml version="1.0" ?>
+        <input type="checkbox" checked="False" />
+        <input type="checkbox" checked="False" />
+        """
+        result = template(checked=False)
+        self.assert_same(expected, result)
+
+    def test_bytes_checked_None(self):
+        template = self.get_template_bytes()
+        expected = """
+        <?xml version="1.0" ?>
+        <input type="checkbox" />
+        <input type="checkbox" />
+        """
+        result = template(checked=None)
+        self.assert_same(expected, result)
+        
+    def test_bytes_checked_default(self):
+        template = self.get_template_bytes()
+        expected = """
+        <?xml version="1.0" ?>
+        <input type="checkbox" checked="nope" />
+        <input type="checkbox" />
+        """
+        result = template(checked=template.default_marker)
+        self.assert_same(expected, result)
+
+    def test_str_checked_true(self):
+        template = self.get_template_str()
+        expected = """
+        <?xml version="1.0" ?>
+        <input type="checkbox" checked="True" />
+        <input type="checkbox" checked="True" />
+        """
+        result = template(checked=True)
+        self.assert_same(expected, result)
+        
+    def test_str_checked_false(self):
+        template = self.get_template_str()
+        expected = """
+        <?xml version="1.0" ?>
+        <input type="checkbox" checked="False" />
+        <input type="checkbox" checked="False" />
+        """
+        result = template(checked=False)
+        self.assert_same(expected, result)
+
+    def test_str_checked_None(self):
+        template = self.get_template_str()
+        expected = """
+        <?xml version="1.0" ?>
+        <input type="checkbox" />
+        <input type="checkbox" />
+        """
+        result = template(checked=None)
+        self.assert_same(expected, result)
+        
+    def test_str_checked_default(self):
+        template = self.get_template_str()
+        expected = """
+        <?xml version="1.0" ?>
+        <input type="checkbox" checked="nope" />
+        <input type="checkbox" />
+        """
+        result = template(checked=template.default_marker)
+        self.assert_same(expected, result)
+        
+class XMLWithEncodingTestCase(BaseTestCase):
+
+    input_bytes = xml_w_enc_bytes
+
+    def test_bytes_encoding(self):
+        template = self.get_template_bytes()
+        self.assertEqual(template.encoding, 'ascii')
+    
+    def test_str_encoding(self):
+        template = self.get_template_str()
+        self.assertEqual(template.encoding, 'ascii')
+
+class HTML5TestCase(BaseTestCase):
+
+    input_bytes = html5_bytes
+
+    def test_bytes_content_type(self):
+        template = self.get_template_bytes()
+        self.assertEqual(template.content_type, None)
+
+    def test_bytes_encoding(self):
+        template = self.get_template_bytes()
+        self.assertEqual(template.encoding, None)
+
+    def test_str_content_type(self):
+        template = self.get_template_str()
+        self.assertEqual(template.content_type, None)
+
+    def test_str_encoding(self):
+        template = self.get_template_str()
+        self.assertEqual(template.encoding, None)
+
+    def test_bytes_checked_true(self):
+        template = self.get_template_bytes()
+        expected = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Title of document</title>
+        </head>
+        <body>
+          <form>
+            <input type="checkbox" checked="checked" />
+            <input type="checkbox" checked="checked" />
+          </form>
+        </body>
+        </html>
+        """
+        result = template(checked=True)
+        self.assert_same(expected, result)
+        
+    def test_bytes_checked_false(self):
+        template = self.get_template_bytes()
+        expected = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Title of document</title>
+        </head>
+        <body>
+          <form>
+            <input type="checkbox" />
+            <input type="checkbox" />
+          </form>
+        </body>
+        </html>
+        """
+        result = template(checked=False)
+        self.assert_same(expected, result)
+
+    def test_bytes_checked_None(self):
+        template = self.get_template_bytes()
+        expected = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Title of document</title>
+        </head>
+        <body>
+          <form>
+            <input type="checkbox" />
+            <input type="checkbox" />
+          </form>
+        </body>
+        </html>
+        """
+        result = template(checked=None)
+        self.assert_same(expected, result)
+        
+    def test_bytes_checked_default(self):
+        template = self.get_template_bytes()
+        expected = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Title of document</title>
+        </head>
+        <body>
+          <form>
+            <input type="checkbox" checked="nope"/>
+            <input type="checkbox" />
+          </form>
+        </body>
+        </html>
+        """
+        result = template(checked=template.default_marker)
+        self.assert_same(expected, result)
+
+    def test_str_checked_true(self):
+        template = self.get_template_str()
+        expected = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Title of document</title>
+        </head>
+        <body>
+          <form>
+            <input type="checkbox" checked="checked" />
+            <input type="checkbox" checked="checked" />
+          </form>
+        </body>
+        </html>
+        """
+        result = template(checked=True)
+        self.assert_same(expected, result)
+        
+    def test_str_checked_false(self):
+        template = self.get_template_str()
+        expected = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Title of document</title>
+        </head>
+        <body>
+          <form>
+            <input type="checkbox" />
+            <input type="checkbox" />
+          </form>
+        </body>
+        </html>
+        """
+        result = template(checked=False)
+        self.assert_same(expected, result)
+
+    def test_str_checked_None(self):
+        template = self.get_template_str()
+        expected = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Title of document</title>
+        </head>
+        <body>
+          <form>
+            <input type="checkbox" />
+            <input type="checkbox" />
+          </form>
+        </body>
+        </html>
+        """
+        result = template(checked=None)
+        self.assert_same(expected, result)
+        
+    def test_str_checked_default(self):
+        template = self.get_template_str()
+        expected = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Title of document</title>
+        </head>
+        <body>
+          <form>
+            <input type="checkbox" checked="nope" />
+            <input type="checkbox" />
+          </form>
+        </body>
+        </html>
+        """
+        result = template(checked=template.default_marker)
+        self.assert_same(expected, result)
+
+class HTML5WithContentTypeAndEncodingTestCase(BaseTestCase):
+
+    input_bytes = html5_w_ct_n_enc_bytes
+
+    def test_bytes_content_type(self):
+        template = self.get_template_bytes()
+        self.assertEqual(template.encoding, 'foo/bar')
+    
+    def test_bytes_encoding(self):
+        template = self.get_template_bytes()
+        self.assertEqual(template.encoding, 'utf-8')
+    
+    def test_str_content_type(self):
+        template = self.get_template_str()
+        self.assertEqual(template.encoding, 'foo/bar')
+
+    def test_str_encoding(self):
+        template = self.get_template_str()
+        self.assertEqual(template.encoding, 'utf-8')
+
