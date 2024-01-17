76b2f365f1d4ad5c1969137a06f6df7b31a149be
Malthe Borch <mborch@gmail.com>
2024-01-17 11:44:30 +0100

    Remove copyright notice

--- src/chameleon/astutil.py
+++ src/chameleon/astutil.py
@@ -1,16 +1,4 @@
-#
-# Copyright (C) 2008-2009 Edgewall Software
-# All rights reserved.
-#
-# This software is licensed as described in the file COPYING, which
-# you should have received as part of this distribution. The terms
-# are also available at http://genshi.edgewall.org/wiki/License.
-#
-# This software consists of voluntary contributions made by many
-# individuals. For the exact contribution history, see the revision
-# history and logs, available at http://genshi.edgewall.org/log/.
-
-"""Support classes for generating code from abstract syntax trees."""
+"""Support code for generating code from abstract syntax trees."""
 
 from __future__ import annotations
 
