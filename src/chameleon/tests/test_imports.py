44b5db8a5e02724c3f4a68fd6fbdd4d1515feb96
Malthe Borch <mborch@gmail.com>
2024-01-18 09:55:48 +0100

    Add compiler sanctioned imports

--- src/chameleon/tests/test_imports.py
+++ src/chameleon/tests/test_imports.py
@@ -9,3 +9,9 @@ class TestImports:
     def test_pagetexttemplates(self):
         from chameleon import PageTextTemplate
         from chameleon import PageTextTemplateFile
+
+    def test_compiler_utils(self):
+        from chameleon.compiler import ExpressionEngine
+        from chameleon.compiler import ExpressionEvaluator
+        from chameleon.tales import ExpressionParser
+        from chameleon.utils import Scope
